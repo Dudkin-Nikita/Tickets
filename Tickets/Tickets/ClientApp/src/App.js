c42153c86c42537d27fdf66dac1f4dae67aadb77
DESKTOP-UUPP9C7\user <nikd926@gmail.com>
2022-11-15 22:31:09 +0300

    Add MySelect to App.js

--- Tickets/Tickets/ClientApp/src/App.js
+++ Tickets/Tickets/ClientApp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import TicketForm from './components/TicketForm';
 import TicketList from './components/TicketList';
+import MySelect from './components/UI/select/MySelect';
 import './styles/App.css'
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
         { id: 3, title: "Ticket 3", body: "OpenAir ticket"}
     ])
 
+    const [selectedSort, setSelectedSort] = useState('')
+
     const createTicket = (newTicket) => {
         setTickets([...tickets, newTicket])
     }
@@ -19,9 +22,26 @@ function App() {
         setTickets(tickets.filter(t => t.id !== ticket.id))
     }
 
+    const sortTickets = (sort) => {
+        setSelectedSort(sort)
+        setTickets([...tickets].sort((a, b) => a[sort].localeCompare(b[sort]))
+    }
+
     return (
         <div className="App">
             <TicketForm create={createTicket} />
+            <hr/>
+            <div>
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortTickets}
+                    defaultValue='Sort'
+                    options={[
+                        { value: 'title', name: 'by name' },
+                        {value: 'body', name: 'by description'}
+                    ]}
+                />
+            </div>
             {tickets.length
                 ? <TicketList remove={removeTicket} tickets={tickets} title="List of tickets" />
                 : <h1>There is no tickets</h1>
