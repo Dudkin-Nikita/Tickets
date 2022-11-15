693a073879899ee1440740f393793c6c64f53cb6
DESKTOP-UUPP9C7\user <nikd926@gmail.com>
2022-11-15 23:47:25 +0300

    Finish sorting by type for tickets.

--- Tickets/Tickets/ClientApp/src/App.js
+++ Tickets/Tickets/ClientApp/src/App.js
@@ -2,29 +2,42 @@ import React, { Component, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import TicketForm from './components/TicketForm';
 import TicketList from './components/TicketList';
+import MyInput from './components/UI/input/MyInput';
 import MySelect from './components/UI/select/MySelect';
 import './styles/App.css'
 
 function App() {
     const [tickets, setTickets] = useState([
-        { id: 1, title: "Ticket", body: "Party ticket" },
-        { id: 2, title: "Ticket 2", body: "Classic ticket" },
-        { id: 3, title: "Ticket 3", body: "OpenAir ticket"}
+        { id: 1, title: "Ticket", body: "Party" },
+        { id: 2, title: "Ticket 2", body: "Classic" },
+        { id: 3, title: "Ticket 3", body: "OpenAir"}
     ])
 
+    const [sortedTickets, setSortedTickets] = useState(tickets)
+
     const [selectedSort, setSelectedSort] = useState('')
+    const [searchQuery, setSearchQuery] = useState('')
+
 
     const createTicket = (newTicket) => {
         setTickets([...tickets, newTicket])
+        if (newTicket.body === selectedSort) {
+            setSortedTickets([...sortedTickets, newTicket])
+        }
     }
 
     const removeTicket = (ticket) => {
         setTickets(tickets.filter(t => t.id !== ticket.id))
+        setSortedTickets(sortedTickets.filter(t => t.id !== ticket.id))
     }
 
     const sortTickets = (sort) => {
         setSelectedSort(sort)
-        setTickets([...tickets].sort((a, b) => a[sort].localeCompare(b[sort]))
+        if (sort !== "All") {
+            setSortedTickets([...tickets].filter(t => t.body === sort))
+        } else {
+            setSortedTickets(tickets)
+        } 
     }
 
     return (
@@ -32,18 +45,25 @@ function App() {
             <TicketForm create={createTicket} />
             <hr/>
             <div>
+                <MyInput
+                    value={searchQuery}
+                    onChange={e => setSearchQuery(e.target.value)}
+                    placeholder="Search"
+                />
                 <MySelect
                     value={selectedSort}
                     onChange={sortTickets}
                     defaultValue='Sort'
                     options={[
-                        { value: 'title', name: 'by name' },
-                        {value: 'body', name: 'by description'}
+                        { value: "All", name: "All" },
+                        { value: "Classic", name: "Classic" },
+                        { value: "Party", name: "Party" },
+                        { value: "OpenAir", name: "OpenAir" }
                     ]}
                 />
             </div>
-            {tickets.length
-                ? <TicketList remove={removeTicket} tickets={tickets} title="List of tickets" />
+            {sortedTickets.length
+                ? <TicketList remove={removeTicket} tickets={sortedTickets} title="List of tickets" />
                 : <h1>There is no tickets</h1>
             } 
         </div>
