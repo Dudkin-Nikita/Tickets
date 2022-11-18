6bfd8d7529eab12c1338dcae6d9306a2cd4ee9ba
DESKTOP-UUPP9C7\user <nikd926@gmail.com>
2022-11-18 19:01:32 +0300

    Separated filter and search to TicketFilter component.

---
+++ Tickets/Tickets/ClientApp/src/components/TicketFilter.jsx
@@ -0,0 +1,28 @@
+﻿import React from 'react';
+import MyInput from './UI/input/MyInput';
+import MySelect from './UI/select/MySelect';
+
+const TicketFilter = ({ filter, setFilter, sortTickets }) => {
+    return (
+        <div>
+            <MyInput
+                value={filter.query}
+                onChange={e => setFilter({ ...filter, query: e.target.value })}
+                placeholder="Search"
+            />
+            <MySelect
+                value={filter.sort}
+                onChange={sortTickets}
+                defaultValue='Sort'
+                options={[
+                    { value: "All", name: "All" },
+                    { value: "Classic", name: "Classic" },
+                    { value: "Party", name: "Party" },
+                    { value: "OpenAir", name: "OpenAir" }
+                ]}
+            />
+        </div>
+    )
+}
+
+export default TicketFilter
